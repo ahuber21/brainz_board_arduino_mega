818f2ae2d13e796a8f18dede623c49644a146161
Andreas Hoenle <der.andi@cern.ch>
2020-08-09 19:40:10 +0200

    Remove idle

--- nano1_firmware/nano1_firmware.ino
+++ nano1_firmware/nano1_firmware.ino
@@ -1,8 +1,8 @@
 /*
-   Arduino Nano 1firmware
-   Communication with Raspberry Pi through I2C
-   The Arduino will operate in slave mode
-*/
+ * Arduino Mega 2560 firmware
+ * Communication with Raspberry Pi through I2C
+ * The Arduino will operate in slave mode
+ */
 
 #include <Wire.h>
 
@@ -10,22 +10,22 @@
 #include "message.h"
 
 /*
-   I2C
-*/
+ * I2C
+ */
 #define I2C_ADDRESS 0x6
 
 
 /*
-   Valve pins
-*/
+ * Valve pins
+ */
 #define VLV1 13
 #define VLV2 12
 #define VLV3 11
 #define VLV4 10
 
 /*
-   Indicator LEDs pins
-*/
+ * Indicator LEDs pins
+ */
 #define LED1R 9
 #define LED1G 8
 #define LED2R 7
@@ -34,14 +34,7 @@
 #define LED3G 4
 #define LED4R 3
 #define LED4G 2
-
-bool STATE1;
-bool STATE2;
-bool STATE3;
-bool STATE4;
-long idle_millis;
-byte idle_idx;
-
+ 
 void setup() {
   // Serial port initialisation
   Serial.begin(9600);
@@ -79,106 +72,18 @@ void setup() {
   digitalWrite(LED2G, LOW);
   digitalWrite(LED3G, LOW);
   digitalWrite(LED4G, LOW);
-
+  
   digitalWrite(LED1R, HIGH);
   digitalWrite(LED2R, HIGH);
   digitalWrite(LED3R, HIGH);
   digitalWrite(LED4R, HIGH);
   Serial.println(F("Pin modes initialised"));
 
-  idle_millis = millis();
-  idle_idx = 0;
-  STATE1 = 0;
-  STATE2 = 0;
-  STATE3 = 0;
-  STATE4 = 0;
-
   Serial.println(F("Arduino Nano ready"));
 }
 
 void loop() {
-  digitalWrite(LED1R, LOW);
-  digitalWrite(LED1G, LOW);
-  digitalWrite(LED2R, LOW);
-  digitalWrite(LED2G, LOW);
-  digitalWrite(LED3R, LOW);
-  digitalWrite(LED3G, LOW);
-  digitalWrite(LED4R, LOW);
-  digitalWrite(LED4G, LOW);
-  if (
-    STATE1 == 0
-    && STATE2 == 0
-    && STATE3 == 0
-    && STATE4 == 0) {
-    if (millis() - idle_millis > 250) {
-      idle_millis = millis();
-      idle_idx = (idle_idx + 1) % 6;
-    }
-    switch (idle_idx) {
-      case 0:
-        digitalWrite(LED1R, LOW);
-        digitalWrite(LED1G, HIGH);
-        digitalWrite(LED2R, HIGH);
-        digitalWrite(LED3R, HIGH);
-        digitalWrite(LED4R, HIGH);
-        break;
-      case 1:
-        digitalWrite(LED1R, HIGH);
-        digitalWrite(LED2R, LOW);
-        digitalWrite(LED2G, HIGH);
-        digitalWrite(LED3R, HIGH);
-        digitalWrite(LED4R, HIGH);
-        break;
-      case 2:
-        digitalWrite(LED1R, HIGH);
-        digitalWrite(LED2R, HIGH);
-        digitalWrite(LED3R, LOW);
-        digitalWrite(LED3G, HIGH);
-        digitalWrite(LED4R, HIGH);
-        break;
-      case 3:
-        digitalWrite(LED1R, HIGH);
-        digitalWrite(LED2R, HIGH);
-        digitalWrite(LED3R, HIGH);
-        digitalWrite(LED4R, LOW);
-        digitalWrite(LED4G, HIGH);
-        break;
-      case 4:
-        digitalWrite(LED1R, HIGH);
-        digitalWrite(LED2R, HIGH);
-        digitalWrite(LED3R, LOW);
-        digitalWrite(LED3G, HIGH);
-        digitalWrite(LED4R, HIGH);
-        break;
-      case 5:
-        digitalWrite(LED1R, HIGH);
-        digitalWrite(LED2R, LOW);
-        digitalWrite(LED2G, HIGH);
-        digitalWrite(LED3R, HIGH);
-        digitalWrite(LED4R, HIGH);
-        break;
-      default:
-        break;
-    }
-  }
-  if (STATE1) {
-    digitalWrite(LED1R, LOW);
-    digitalWrite(LED1G, HIGH);
-  }
-  if (STATE2) {
-    digitalWrite(LED2R, LOW);
-    digitalWrite(LED2G, HIGH);
-  }
-  if (STATE3) {
-    digitalWrite(LED3R, LOW);
-    digitalWrite(LED3G, HIGH);
-  }
-  if (STATE4) {
-    digitalWrite(LED4R, LOW);
-    digitalWrite(LED4G, HIGH);
-  }
-
-  delay(5);
+  delay(100);
 }
 
 void receive(int byteCount) {
@@ -190,35 +95,43 @@ void receive(int byteCount) {
     switch (op_code) {
       case OC_VLV1_OPEN:
         digitalWrite(VLV1, LOW);
-        STATE1 = 1;
+        digitalWrite(LED1G, HIGH);
+        digitalWrite(LED1R, LOW);
         break;
       case OC_VLV1_CLOSE:
         digitalWrite(VLV1, HIGH);
-        STATE1 = 0;
+        digitalWrite(LED1G, LOW);
+        digitalWrite(LED1R, HIGH);
         break;
       case OC_VLV2_OPEN:
         digitalWrite(VLV2, LOW);
-        STATE2 = 1;
+        digitalWrite(LED2G, HIGH);
+        digitalWrite(LED2R, LOW);
         break;
       case OC_VLV2_CLOSE:
         digitalWrite(VLV2, HIGH);
-        STATE2 = 0;
+        digitalWrite(LED2G, LOW);
+        digitalWrite(LED2R, HIGH);
         break;
       case OC_VLV3_OPEN:
         digitalWrite(VLV3, LOW);
-        STATE3 = 1;
+        digitalWrite(LED3G, HIGH);
+        digitalWrite(LED3R, LOW);
         break;
       case OC_VLV3_CLOSE:
         digitalWrite(VLV3, HIGH);
-        STATE3 = 0;
+        digitalWrite(LED3G, LOW);
+        digitalWrite(LED3R, HIGH);
         break;
       case OC_VLV4_OPEN:
         digitalWrite(VLV4, LOW);
-        STATE4 = 1;
+        digitalWrite(LED4G, HIGH);
+        digitalWrite(LED4R, LOW);
         break;
       case OC_VLV4_CLOSE:
         digitalWrite(VLV4, HIGH);
-        STATE4 = 0;
+        digitalWrite(LED4G, LOW);
+        digitalWrite(LED4R, HIGH);
         break;
       default:
         Serial.print(F("ERR:OP_CODE_UNKNOWN="));
@@ -226,7 +139,7 @@ void receive(int byteCount) {
         break;
     }
   }
-}
+} 
 
 void request() {
   if (message.ready) {
